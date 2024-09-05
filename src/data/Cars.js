99511e6a25279ce35b9103cd852eb6defed40405
bopi1829 <bopi1829@gmail.com>
2024-09-05 11:21:17 +0200

    ajout des fichiers

---
+++ src/data/Cars.js
@@ -0,0 +1,20 @@
+const Cars = [
+  {
+    marque: "Renault", modele: "Clio", prix: "8 990 €", energie: "Essence", stocked: true,
+  },
+  {
+    marque: "Peugeot", modele: "208", prix: "9 490 €", energie: "Diesel", stocked: false,
+  },
+  {
+    marque: "Citroën", modele: "C3", prix: "12 990 €", energie: "Electrique", stocked: true,
+  },
+  {
+    marque: "Volkswagen", modele: "Polo", prix: "9 990 €", energie: "Essence", stocked: false,
+  },
+  {
+    marque: "Skoda", modele: "Fabia", prix: "7 890 €", energie: "Diesel", stocked: true,
+  },
+  {
+    marque: "Audi", modele: "A1", prix: "13 590 €", energie: "Diesel", stocked: true,
+  }
+]
